5c1e63479524ac5896585e8280df5fa135d59c4f
vvvoltalelystad <jaccox10@gmail.com>
2026-08-15 10:48:54 +0200

    Restore Excel import modal styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AuthGate } from './AuthGate'
 import './styles.css'
+import './import.css'
 import './auth.css'
 
 createRoot(document.getElementById('root')!).render(
